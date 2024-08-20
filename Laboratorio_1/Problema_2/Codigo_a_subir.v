0fb46994183e08606a054e263b3fc4a6eb170c6d
Jsuare <126419416+Jsuare@users.noreply.github.com>
2024-08-19 20:37:51 -0600

    Revert "rama ejercico 3 en proceso"

--- Laboratorio_1/Problema_2/Codigo_a_subir.v
+++ Laboratorio_1/Problema_2/Codigo_a_subir.v
@@ -1,9 +1,9 @@
 module top (
-    input wire [3:0] numero,   // Entrada del número (0 a 15)
-    output reg [3:0] led       // Salida del complemento a 2
+    input wire clk,                // Entrada de reloj
+    input wire [3:0] numero, // Entrada del número (0 a 15)
+    output reg [3:0] leds         // Salida del complemento a 2
 );
-    reg [3:0] complemento;
-    assign complemento = ~numero + 1;   // Calcular el complemento a 2
-    assign led = complemento;                 // Asignar el valor de complemento a `led`
-
+always @(*) begin
+   leds = ~numero - 1;                    // Asignar el valor de complemento a leds
+end
 endmodule
